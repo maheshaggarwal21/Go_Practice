5589e78e66b5e7c9522863ef47b8e0cec0c225d5
Mahesh Aggarwal <developermahesh21@gmail.com>
2025-03-13 01:46:46 +0530

    Added helloWorld file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maheshaggarwal21/Go_Practice.git
+
+go 1.24.1
---
+++ helloWorld.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// This is a single line comment
+/* This is a 
+multiline comment */
+
+func main(){
+	fmt.Println("Hellllooo Worlddd!!!!")
+}
